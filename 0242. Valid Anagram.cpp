32c5e5e816faaaec89d9bef84b8c02d6295b2b1e
Soumya Maheshwari <104725768+soumya-maheshwari@users.noreply.github.com>
2023-03-06 12:59:10 +0530

    created 0242. Valid Anagram.cpp

---
+++ 0242. Valid Anagram.cpp
@@ -0,0 +1,11 @@
+242. Valid Anagram
+
+class Solution {
+public:
+    bool isAnagram(string a, string b) {
+        sort(a.begin(),a.end());
+       sort(b.begin(),b.end());
+       
+       return (a==b);
+    }
+};
